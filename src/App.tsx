e73b3921d092f454c29b8ad9b5f4e223f51362b1
AdrianMorari <81621698+AdrianMorari@users.noreply.github.com>
2022-03-25 17:38:08 +0200

    adding deleted project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import IndexGenres from "./genres/IndexGenres";
+import Menu from "./Menu";
+import { landingPageDTO } from "./movies/movies.model";
+import MoviesList from "./movies/MoviesList";
 
 function App() {
+  const [movies, setMovies] = useState<landingPageDTO>({});
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setMovies({
+        inTheaters: [
+          {
+            id: 1,
+            title: "Spider-Man: Far From Home",
+            poster:
+              "https://upload.wikimedia.org/wikipedia/en/b/bd/Spider-Man_Far_From_Home_poster.jpg",
+          },
+          {
+            id: 2,
+            title: "Luca",
+            poster:
+              "https://upload.wikimedia.org/wikipedia/en/thumb/3/33/Luca_%282021_film%29.png/220px-Luca_%282021_film%29.png",
+          },
+        ],
+        upcomingReleases: [
+          {
+            id: 3,
+            title: "Soul",
+            poster:
+              "https://upload.wikimedia.org/wikipedia/en/3/39/Soul_%282020_film%29_poster.jpg",
+          },
+        ],
+      });
+    }, 1000);
+
+    return () => clearTimeout(timerId);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Menu />
+        <div className="container">
+          <Switch>
+            <Route exact path="/">
+              <h3>In Theaters</h3>
+              <MoviesList movies={movies.inTheaters} />
+              <hr />
+              <h3>Upcoming Releases</h3>
+              <MoviesList movies={movies.upcomingReleases} />
+            </Route>
+
+            <Route exact path="/genres">
+              <IndexGenres />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/utils/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <>
+      <img alt="loading" src="https://i.gifer.com/Yb3A.gif" />
+    </>
+  );
+}
